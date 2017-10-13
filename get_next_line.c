436209c2b3afd0972a30211b5123900919b1550a
Joshua Meier <jmeier@e1z2r9p40.42.us.org>
2017-10-13 15:04:35 -0700

    Changed up gnl.  It segfaults, but I can look at how the logic works and see if there are any gaps in my understanding.  Worst comes worst, I ask around to see how it works

--- get_next_line.c
+++ get_next_line.c
@@ -6,69 +6,65 @@
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/30 13:57:43 by jmeier            #+#    #+#             */
-/*   Updated: 2017/10/05 23:07:07 by jmeier           ###   ########.fr       */
+/*   Updated: 2017/10/11 21:46:35 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+# define BUF_SIZE 4096
 
-int		a_function_to_look_through_whatever_has_been_added_to_the_buffer, specifically for 0s and newline characrters(char *line, int size)
-{
-	if (!ft_strchr(line, '\n'))
-		return (0);
-	
-
-
-
-
-
-
-
-
-
-
-char	*read_input(const int fd)
-{
-	char	*init_buf;
-	char	*read_buf;
-	int		b;
-	int		r;
-	int		l;
-
-	init_buf = ft_strnew(BUF_SIZE);
-	b = 0;
-	while ((r = read(fd, init_buf + b, BUF_SIZE - b)) > 0)
-		b += r;
-	l = 0;
-	while (l < r && ((init_buf[l] != '\n') || (init_buf[l] != '0')))
-		l++;
-	ft_strncpy(read_buf, init_buf, l);
-	free(init_buf);
-	return (read_buf);
-}
-
-I need to find a way to stuff the line i've found into the **line parameter
-To this end, read_input shall now just find the crap I need to read, stuff that output into a separate buffer.  After that buffer has been 
-
-
-int		get_next_line(const int fd, char **line)
+static int		search(char *line, int r, char c)
 {
 	int		i;
 
-	*line = read_input(fd);
-	if (ft_strlen(*line) == BUF_SIZE)
-		return (1);
-	if (ft_strlen(*line) > BUF_SIZE)
-		return (0);
-	return (-1);
+	i = 0;
+	if (c == '0')
+	{
+		while ((i + 2 < r) && (line[i] != c) && (line[i + 1] != c) &&
+			(line[i + 2] != c))
+			i++;
+	}
+	else
+	{
+		while ((i < r) && (line[i] != c))
+			i++;
+	}
+	return (i);
 }
 
-int		main()
+static char	*affix(char *line, char *buf, int i)
 {
-	char *line;
+	char	*read_buf;
+	int		j;
 
-	while (get_next_line(1, &line))
+	j = 0;
+	read_buf = ft_strnew(i - 1);
+	while (j++ < i)
+		read_buf[j] = buf[j];
+	free(buf);
+	line = ft_strjoin(line, read_buf);
+	free(read_buf);
+	return (line);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	char	*init_buf;
+	int		r;
+	int		i;
+
+	init_buf = ft_strnew(BUF_SIZE);
+	r = read(fd, init_buf, BUF_SIZE);
+	if (r < 0)
+		return (-1);
+	r = 0;
+	if (!ft_strchr(init_buf, '\n'))
 	{
-		;
+		i = search(init_buf, r, '0');
+		affix(*line, init_buf, i);
+		return (0);
 	}
+	i = search(init_buf, r, '\n');
+	affix(*line, init_buf, i);
+	return (1);
 }
