ba3f4c931ba8b7dc36ed83ea3744454841580a70
Joshua Meier <jmeier@e1z3r4p24.42.us.org>
2017-11-20 14:19:21 -0800

    New and improved, with even more macros

--- get_next_line.c
+++ get_next_line.c
@@ -5,114 +5,83 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/30 13:57:43 by jmeier            #+#    #+#             */
-/*   Updated: 2017/10/24 00:49:29 by jmeier           ###   ########.fr       */
+/*   Created: 2017/10/25 09:34:13 by jmeier            #+#    #+#             */
+/*   Updated: 2017/11/20 14:18:49 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static int		parse(t_block **block, char *line)
+static int			parse(t_block **block, char **line)
 {
 	int i;
 
 	i = 0;
-	while ((*block)->str[i] != '\n' && (*block)->str[i] != '\0')
+	while ((*block)->str[i] && (*block)->str[i] != '\n')
 		i++;
 	if ((*block)->str[i] == '\n')
 	{
-		ASSERT(line = ft_strsub((*block)->str, 0, i));
+		ASSERT(*line = ft_strsub((*block)->str, 0, i));
 		ASSERT((*block)->tmp = ft_strdup((*block)->str + i + 1));
 		free((*block)->str);
 		(*block)->str = (*block)->tmp;
+		free((*block)->tmp);
+		(*block)->tmp = NULL;
 	}
 	else if (!(*block)->str[i])
 	{
 		if (i == 0)
 			return (0);
-		ASSERT(line = ft_strdup((*block)->str));
+		ASSERT(*line = ft_strdup((*block)->str));
 		free((*block)->str);
 		(*block)->str = NULL;
 	}
 	return (1);
 }
 
-/*
-** Populates the struct member str which stores the read buffer and is to be
-** parsed in a later function.  The variable buffer is not used for those
-** purposes because it's much more convenient to keep the contents in a struct
-** and continually manipulate it if someone puts a PITA buffer size like 1 and
-** calls GNL several times.
-** This function is followed by a strchr to affirm whether or not the buffer
-** that was stored contains a newline char or not.  If not, then the loop
-** should continue and more stuff will get appended to str via strjoin.
-** If someone could suggest something train related to call this and still keep
-** a sense of what this is supposed to do, I'd be grateful.
-*/
-
-static int		fill_str(t_block *block, char *buf)
+static t_block	*cycle_fd(t_block **train, const int fd)
 {
-	if (!block->str)
-		return (0);
-	if (!(block->tmp = ft_strjoin(block->str, buf)))
-		return (0);
-	free(block->str);
-	block->str = NULL;
-	if (!(block->str = ft_strdup(block->tmp)))
-		return (0);
-	free(block->tmp);
-	block->tmp = NULL;
-	return (1);
-}
+	t_block			*tmp;
 
-/*
-** Due to how I initially visualized GNL, as a bunch of cars being shunted
-** around and coupled to the end until there is a caboose added, the train
-** theme is somewhat prevalent.  Trains are really cool.  I wrote this with the
-** intent of shifting through lists as though cycling through a train
-** roundhouse on a turntable.
-** C_ASSERT works as ASSERT does, although it returns NULL instead.  The C
-** stands for "Char."
-*/
-
-static t_block	*station_fd(t_block **train, const int fd)
-{
-	t_block			*new;
-
-	new = *train;
-	while (new && new->fd != fd)
-		new = new->next;
-	if (!new)
+	tmp = *train;
+	while (tmp && tmp->fd != fd)
+		tmp = tmp->next;
+	if (!tmp)
 	{
-		new = (t_block *)ft_memalloc(sizeof(t_block));
-		new->fd = fd;
-		C_ASSERT(new->str = ft_strnew(0));
-		new->next = *train;
-		*train = new;
+		C_ASSERT(tmp = (t_block *)ft_memalloc(sizeof(t_block)));
+		tmp->fd = fd;
+		C_ASSERT(tmp->str = ft_strnew(0));
+		tmp->next = *train;
+		*train = tmp;
 	}
-	return (new);
+	return (tmp);
 }
 
 int				get_next_line(const int fd, char **line)
 {
 	static t_block	*train;
 	t_block			*tmp;
-	char			buffer[BUFF_SIZE + 1];
+	char			buf[BUFF_SIZE + 1];
+	int				len;
 
 	ASSERT(fd >= 0 || line);
-	tmp = station_fd(&train, fd);
-	while ((tmp->len = read(tmp->fd, &buffer, BUFF_SIZE)) > 0)
+	tmp = cycle_fd(&train, fd);
+	while ((len = read(tmp->fd, &buf, BUFF_SIZE)) > 0)
 	{
-		buffer[tmp->len] = '\0';
-		ASSERT(fill_str(tmp, buffer));
-		if (ft_strchr(buffer, '\n'))
+		buf[len] = '\0';
+		ASSERT(tmp->tmp = ft_strjoin(tmp->str, buf));
+		free(tmp->str);
+		ASSERT(tmp->str = ft_strdup(tmp->tmp));
+		free(tmp->tmp);
+		tmp->tmp = NULL;
+		if (ft_strchr(buf, '\n'))
 			break ;
 	}
-	ASSERT(tmp->len >= 0);
-	if (!tmp->len && !tmp->str)
+	ASSERT(len >= 0);
+	if (!len && !tmp->str)
 	{
 		tmp->fd = 0;
 		return (0);
 	}
-	return (parse(&tmp, *line));
+	return (parse(&tmp, line));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -5,16 +5,29 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: jmeier <jmeier@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2017/09/30 13:53:47 by jmeier            #+#    #+#             */
-/*   Updated: 2017/10/11 21:44:51 by jmeier           ###   ########.fr       */
+/*   Created: 2017/10/25 09:05:17 by jmeier            #+#    #+#             */
+/*   Updated: 2017/10/29 19:52:49 by jmeier           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# include <libft/libft.h>
-# define BUF_SIZE 4096
+# include <stdlib.h>
+# include <fcntl.h>
+# include "libft/libft.h"
+# define BUFF_SIZE 32
+# define ASSERT(a) if (!(a)) return (-1);
+# define C_ASSERT(a) if (!(a)) return (NULL);
 
-int		get_next_line(const int fd, char **line);
+typedef struct		s_block
+{
+	char			*str;
+	char			*tmp;
+	int				len;
+	int				fd;
+	struct s_block	*next;
+}					t_block;
+
+int					get_next_line(const int fd, char **line);
 
 #endif
